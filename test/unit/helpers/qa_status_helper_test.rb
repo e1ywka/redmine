ded0b6b6e93f16826732873d438439d0728dc188
Ivan Agafontsev <e1ywka@gmail.com>
2013-01-09 17:47:31 +0400

    add new fixtures

--- test/unit/helpers/qa_status_helper_test.rb
+++ test/unit/helpers/qa_status_helper_test.rb
@@ -2,10 +2,10 @@ require File.expand_path('../../../test_helper', __FILE__)
 
 class CustomFieldsHelperTest < ActionView::TestCase
   include QaStatusHelper
-  ISSUE_ID
+  ISSUE_ID = 1
 
   def test_issue_status
-    qaHelper = QaStatusHelper.new
     issue = Issue.find(ISSUE_ID)
+    assert_not_nil issue
   end
 end
