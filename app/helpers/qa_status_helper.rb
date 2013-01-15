201279082935f0f9277bf7158087a4427f913199
Ivan Agafontsev <e1ywka@gmail.com>
2013-01-15 17:57:12 +0400

    relates_to_testers helper method

--- app/helpers/qa_status_helper.rb
+++ app/helpers/qa_status_helper.rb
@@ -7,10 +7,18 @@ module QaStatusHelper
 
   # Возвращает Issue, которое связано с параметром issue, имеет указанный трекер и указанное настраеваемое поле
   # Если условиям удовлетворяет несколько тикетов, берется тикет с наибольшей версией
+  # @param [Issue] issue
   def relates_to_testers(issue, params)
     # Проверяем наличие параметров в хэше: tracker_name, custom_field_name
+    return if ((params[:tracker_name] == nil) || (params[:custom_field_name] == nil))
     # Выбираем тикеты с указанным трекером и кастомным полем
+    return if issue.relations_to.empty?
+    related_issues = issue.relations_to.select { |iss_rel|
+      iss_rel.other_issue(issue).tracker.name == params[:tracker_name]
+    }
+
     # При наличии нескольких тикетов сравниваем версии и выбираем с большей
+    return related_issues[0]
   end
 
   # Выбираем тикет с наибольшей версией
--- test/unit/helpers/qa_status_helper_test.rb
+++ test/unit/helpers/qa_status_helper_test.rb
@@ -21,4 +21,53 @@ class QaStatusHelperTest < ActionView::TestCase
     puts issue.custom_field_values
     assert_equal "User_firstname User_lastname, status name", issue_status(issue)
   end
+
+  def test_relates_to_testers_method_params
+    issue = Issue.new
+    issue.tracker_id = 1
+    issue.project_id = 1
+    issue.subject = "Issue 1"
+
+    assert_nil relates_to_testers(issue, :tracker_name => "Some name")
+
+    tracker = Tracker.new
+    tracker.name = "Some name"
+
+    issue2 = Issue.new
+    issue2.tracker = tracker
+    issue2.project_id = 1
+    issue2.subject = "Issue 2"
+
+    issue_relation = IssueRelation.new
+    issue_relation.issue_from = issue
+    issue_relation.issue_to = issue2
+
+    issue.relations_to.push issue_relation
+    assert_not_nil relates_to_testers(issue, :tracker_name => "Some name", :custom_field_name => "Some field name")
+  end
+
+  def test_relates_to_testers_correct_ticket
+    issue = Issue.new
+    issue.tracker_id = 1
+    issue.project_id = 1
+    issue.subject = "Issue 1"
+
+    tracker = Tracker.new
+    tracker.name = "Some name"
+
+    issue2 = Issue.new
+    issue2.tracker = tracker
+    issue2.project_id = 1
+    issue2.subject = "Issue 2"
+
+    issue_relation = IssueRelation.new
+    issue_relation.issue_from = issue
+    issue_relation.issue_to = issue2
+
+    issue.relations_to.push issue_relation
+    searched_issue = relates_to_testers(issue, :tracker_name => "Some name", :custom_field_name => "Some field name")
+    assert_not_nil searched_issue
+    assert !searched_issue.kind_of?(Array)
+    assert issue2 == searched_issue
+  end
 end
